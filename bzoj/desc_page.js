2b68a024b3d11f5bec087cce7d05a100a3f919d8
xmcp <xiaoyuanan2000@sina.com>
2017-05-22 23:55:59 +0800

    v1.6

--- bzoj/desc_page.js
+++ bzoj/desc_page.js
@@ -15,7 +15,13 @@ function add_btn(text,href,newtab) {
 
 // http://stackoverflow.com/questions/175739/is-there-a-built-in-way-in-javascript-to-check-if-a-string-is-a-valid-number
 if(titleobj && probid && !isNaN(probid)) {
-    titleobj.appendChild(add_btn('[Search]','https://www.baidu.com/s?wd=bzoj%20'+probid,true));
+    var container=document.createElement('p');
+    container.style.fontWeight='bold';
+    container.appendChild(add_btn('[Search]','https://www.baidu.com/s?wd=bzoj%20'+probid,true));
+    container.appendChild(add_btn('[All Status]','/JudgeOnline/status.php?problem_id='+probid));
     if(username)
-        titleobj.appendChild(add_btn('[My Status]','/JudgeOnline/status.php?problem_id='+probid+'&user_id='+username.textContent));
+        container.appendChild(add_btn('[My Status]','/JudgeOnline/status.php?problem_id='+probid+'&user_id='+username.textContent));
+    
+    // https://stackoverflow.com/questions/4793604/how-to-do-insert-after-in-javascript-without-using-a-library
+    titleobj.parentNode.insertBefore(container,titleobj);
 }
--- bzoj/global.js
+++ bzoj/global.js
@@ -1,16 +1,34 @@
-var username=document.querySelector('a[href^="userinfo.php"] font[color="red"]');
+var username=document.querySelector('a[href*="userinfo.php"] font[color="red"]');
 var status_header=document.querySelector('tr.hd a[href*="status.php"]');
+var problems_header=document.querySelector('tr.hd a[href*="problemset.php"]');
 
-function add_btn(text,href,newtab) {
+function add_btn(text,href,callback) {
     var btn=document.createElement('a');
     btn.href=href;
-    if(newtab)
-        btn.target="_blank";
+    if(callback)
+        btn.addEventListener('click',callback);
     btn.textContent=text;
     btn.style.color="red";
     return btn;
 }
 
+['faqs.php','donation.php'].forEach(function(url) {
+    var obj=document.querySelector('tr.hd a[href*="'+url+'"]');
+    if(obj)
+        obj.style.display='none';
+});
+
+function bzoj_search() {
+    alert(1);
+}
+
 if(username && status_header) {
     status_header.parentNode.insertBefore(add_btn('[My]','/JudgeOnline/status.php?user_id='+username.textContent),status_header);
+}
+if(problems_header) {
+    problems_header.parentNode.insertBefore(add_btn('[Search]','#',function() {
+        var res=prompt('Search:');
+        if(res)
+            location.href='/JudgeOnline/problemset.php?search='+encodeURIComponent(res);
+    }),problems_header);
 }
