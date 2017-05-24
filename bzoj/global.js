63597741da2bd39fc8f8de8feb6b87d4d83a9493
xmcp <xiaoyuanan2000@sina.com>
2017-05-24 22:15:46 +0800

    v1.7
    add bzoj auto refresh

--- bzoj/global.js
+++ bzoj/global.js
@@ -12,16 +12,32 @@ function add_btn(text,href,callback) {
     return btn;
 }
 
+// resolve 5xx error
+if(document.title.charAt(0)==='5' && document.body && document.body.getAttribute('bgcolor')==='white') {
+    var cancelbtn=document.createElement('button');
+    cancelbtn.textContent=' Stop Auto Refreshing ';
+    cancelbtn.style.fontSize='1.25em';
+    cancelbtn.style.padding='.25em';
+    document.body.appendChild(cancelbtn);
+    var refreshid=setTimeout(function() {
+        cancelbtn.textContent='Refreshing...';
+        cancelbtn.setAttribute('disabled','disabled');
+        location.reload();
+    } ,1000);    
+    cancelbtn.addEventListener('click',function() {
+        clearTimeout(refreshid);
+        cancelbtn.parentNode.removeChild(cancelbtn);
+    });
+}
+
+// hide link
 ['faqs.php','donation.php'].forEach(function(url) {
     var obj=document.querySelector('tr.hd a[href*="'+url+'"]');
     if(obj)
         obj.style.display='none';
 });
 
-function bzoj_search() {
-    alert(1);
-}
-
+// add link
 if(username && status_header) {
     status_header.parentNode.insertBefore(add_btn('[My]','/JudgeOnline/status.php?user_id='+username.textContent),status_header);
 }
